77ca6d974e2c13f2b11d8160dc5ab744dd8130eb
Allison Grekin <allison_grekin@hotmail.com>
2017-02-16 17:25:02 +0000

    submitting version 1 of cocktail site

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
+
+  # validates :description, presence: true
+  # validates :cocktail_id, presence: true, uniqueness: true
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  # get 'doses/new'
+
+  # get 'cocktails/index'
+
+  # get 'cocktails/edit'
+
+  # get 'cocktails/new'
+
+  # get 'cocktails/show'
+
+  # get 'cocktails/_form'
+
+  resources :cocktails do
+    resources :doses , only: [:new, :create, :destroy]
+  end
+
+  root to: 'cocktails#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
